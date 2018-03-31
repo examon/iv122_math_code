e9a4e6797a7d60b2131ba44c9e9b08970972f54b
Tomas Meszaros <exo@tty.sk>
2018-03-31 18:10:14 +0200

    add: class 6 trees

--- class_6/README.md
+++ class_6/README.md
@@ -2,11 +2,23 @@
 
 ---
 
+## Chaos Game
+
+TODO
+
+---
+
+## Feigenbaum Diagram
+
+TODO
+
+---
+
 ## L-Systems
 
 
 ### Koch Snowflake
-[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L40)
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L65)
 
 [animation](code/img/koch_snowflake_animate.svg)
 
@@ -21,7 +33,7 @@ depth = 6
 
 
 ### Sierpinski Triangle
-[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L52)
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L78)
 
 [animation](code/img/sierpinski_triangle_animate.svg)
 
@@ -36,7 +48,7 @@ depth = 8
 
 
 ### Hilbert Curve
-[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L65)
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L91)
 
 [animation](code/img/hilbert_curve_animate.svg)
 
@@ -51,3 +63,71 @@ depth = 4
 ![lines](code/img/hilbert_curve.svg)
 
 
+### Trees
+
+#### Basic Tree
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L105)
+
+[animation](code/img/basic_tree_animate.svg)
+
+```
+axiom = "A"
+rules = {"A": "F[+A]-A", "F": "FF"}
+length = 1
+angle = 30
+interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+depth = 9
+```
+
+![basic_tree](code/img/basic_tree.svg)
+
+
+#### Fancy Tree 1
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L120)
+
+[animation](code/img/fancy_tree_1_animate.svg)
+
+```
+axiom = "F"
+rules = {"F": "F[+F]F[-F]F"}
+length = 3
+angle = 25.7
+interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+depth = 5
+```
+
+![fancy_tree_1](code/img/fancy_tree_1.svg)
+
+
+#### Fancy Tree 2
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L134)
+
+[animation](code/img/fancy_tree_2_animate.svg)
+
+```
+axiom = "F"
+rules = {"F": "F[+FF]F[-FF]F"}
+length = 3
+angle = 25.7
+interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+depth = 5
+```
+
+![fancy_tree_2](code/img/fancy_tree_2.svg)
+
+
+#### Stochastic Tree
+[code](https://github.com/examon/iv122_math_code/blob/4091dad0481f3196c27dbe586036528392617243/class_6/code/lsystems.py#L148)
+
+```
+axiom = "F"
+rules = {"F": ["[+F]F[−F]", "F[+F]F", "F[-F]F"]}
+length = 5
+angle = [25, 30, 35]
+interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+depth = 9
+```
+
+![stochastic](code/img/fancy_tree_3_stochastic.svg)
+![stochastic_2](code/img/fancy_tree_3_stochastic_2.svg)
+![stochastic_3](code/img/fancy_tree_3_stochastic_3.svg)
--- class_6/code/lsystems.py
+++ class_6/code/lsystems.py
@@ -4,25 +4,35 @@ Tomas Meszaros
 L-Systems
 """
 
+import random
+
 from myturtle import Turtle
 
 
-def l_system_draw(turtle, img_out, axiom, rules, interpretation, depth=2):
+def l_system_draw(turtle, img_out, axiom, rules, interpretation, depth=2, debug=False):
     def expand(axiom, rules, interpretation, depth=2):
         word = axiom
-        #print(word)
+        if debug:
+            print(word)
         for _ in range(depth):
             cur = ""
             for ch in word:
                 if ch in rules:
-                    cur += rules[ch]
+                    rule = rules[ch]
+                    ## Determine weather we need to use stochastic rules
+                    if type(rule) is list:
+                        cur += rule[random.choice(range(len(rule)))]
+                    else:
+                        cur += rule
                 else:
                     cur += ch
             word = cur
-            #print(word)
+            if debug:
+                print(word)
         return word
 
     expanded = expand(axiom, rules, interpretation, depth)
+    turtle_position = list()
     for symbol in expanded:
         if symbol not in interpretation:
             continue
@@ -30,9 +40,25 @@ def l_system_draw(turtle, img_out, axiom, rules, interpretation, depth=2):
         if fcn == "forward":
             turtle.forward(by)
         elif fcn == "right":
-            turtle.right(by)
+            if type(by) is list:
+                turtle.right(by[random.choice(range(len(by)))])
+            else:
+                turtle.right(by)
         elif fcn == "left":
-            turtle.left(by)
+            if type(by) is list:
+                turtle.left(by[random.choice(range(len(by)))])
+            else:
+                turtle.left(by)
+        elif fcn == "stack":
+            if by == "push":
+                turtle_position.append((turtle.get_x(), turtle.get_y(), turtle.get_angle()))
+            elif by == "pop":
+                x, y, angle = turtle_position.pop()
+                turtle.set_x(x)
+                turtle.set_y(y)
+                turtle.set_angle(angle)
+            else:
+                raise Exception("Error: wrong stack instruction", by)
         else:
             raise Exception("Error: wrong instruction", fcn)
     turtle.save(img_out)
@@ -47,7 +73,7 @@ def draw_koch_snowflake():
     turtle.set_x(150)
     turtle.set_y(300)
     l_system_draw(turtle, "img/koch_snowflake.svg", axiom, rules, interpretation, depth)
-draw_koch_snowflake()
+#draw_koch_snowflake()
 
 def draw_sierpinski_triange():
     turtle = Turtle(width=1100, height=1000, center_origin=True, show_borders_and_origin=False, animate=False, animation_speed=0.001)
@@ -60,7 +86,7 @@ def draw_sierpinski_triange():
     turtle.set_x(500)
     turtle.set_y(-500)
     l_system_draw(turtle, "img/sierpinski_triangle.svg", axiom, rules, interpretation, depth)
-draw_sierpinski_triange()
+#draw_sierpinski_triange()
 
 def draw_hilbert_curve():
     turtle = Turtle(width=1000, height=1000, center_origin=True, show_borders_and_origin=False, animate=False, animation_speed=0.001)
@@ -74,4 +100,61 @@ def draw_hilbert_curve():
     turtle.set_x(400)
     turtle.set_y(-400)
     l_system_draw(turtle, "img/hilbert_curve.svg", axiom, rules, interpretation, depth)
-draw_hilbert_curve()
+#draw_hilbert_curve()
+
+def draw_basic_tree():
+    turtle = Turtle(width=600, height=600, center_origin=True, show_borders_and_origin=False, animate=False, animation_speed=0.001)
+    axiom = "A"
+    rules = {"A": "F[+A]-A", "F": "FF"}
+    length = 1
+    angle = 30
+    interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+    depth = 9
+
+    turtle.left(90)
+    turtle.set_y(-300)
+    l_system_draw(turtle, "img/basic_tree.svg", axiom, rules, interpretation, depth)
+#draw_basic_tree()
+
+
+def draw_fancy_tree_1():
+    turtle = Turtle(width=500, height=800, center_origin=True, show_borders_and_origin=False, animate=False, animation_speed=0.001)
+    axiom = "F"
+    rules = {"F": "F[+F]F[-F]F"}
+    length = 3
+    angle = 25.7
+    interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+    depth = 5
+
+    turtle.left(90)
+    turtle.set_y(-400)
+    l_system_draw(turtle, "img/fancy_tree_1.svg", axiom, rules, interpretation, depth)
+#draw_fancy_tree_1()
+
+def draw_fancy_tree_2():
+    turtle = Turtle(width=700, height=1000, center_origin=True, show_borders_and_origin=False, animate=False, animation_speed=0.001)
+    axiom = "F"
+    rules = {"F": "F[+FF]F[-FF]F"}
+    length = 3
+    angle = 25.7
+    interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+    depth = 5
+
+    turtle.left(90)
+    turtle.set_y(-500)
+    l_system_draw(turtle, "img/fancy_tree_2.svg", axiom, rules, interpretation, depth)
+#draw_fancy_tree_2()
+
+def draw_fancy_tree_3_stochastic():
+    turtle = Turtle(width=800, height=1000, center_origin=True, show_borders_and_origin=False, animate=False, animation_speed=0.001)
+    axiom = "F"
+    rules = {"F": ["[+F]F[−F]", "F[+F]F", "F[-F]F"]}
+    length = 5
+    angle = [25, 30, 35]
+    interpretation = {"F": ("forward", length), "[": ("stack", "push"), "]": ("stack", "pop"), "+": ("right", angle), "-": ("left", angle)}
+    depth = 9
+
+    turtle.left(90)
+    turtle.set_y(-500)
+    l_system_draw(turtle, "img/fancy_tree_3_stochastic.svg", axiom, rules, interpretation, depth, debug=True)
+#draw_fancy_tree_3_stochastic()
